4a19f4675c4b293f22a9bb0cf996c095b0fd497e
Lord-Meca <lordmeca255@gmail.com>
2024-12-04 03:37:00 +0100

    update

---
+++ lua/wos/anim_extension/holdtypes/weapon_art2.lua
@@ -0,0 +1,23 @@
+
+local DATA = {}
+
+DATA.Name = "Weapon Art 2"
+DATA.HoldType = "weapon_art2"
+DATA.BaseHoldType = "melee2"
+DATA.Translations = {}
+
+DATA.Translations[ ACT_MP_STAND_IDLE ]					= "b_idle"
+DATA.Translations[ ACT_MP_WALK ]						= "walk_charactercustom"
+DATA.Translations[ ACT_MP_RUN ]							= "run_charactercustom"
+DATA.Translations[ ACT_MP_CROUCH_IDLE ]					= "couch_idle"
+DATA.Translations[ ACT_MP_CROUCHWALK ]					= "cwalk_melee1"
+
+DATA.Translations[ ACT_MP_ATTACK_STAND_PRIMARYFIRE ]	= "foc_jiraya_dbhouse" 
+DATA.Translations[ ACT_MP_ATTACK_CROUCH_PRIMARYFIRE ]	= "foc_jiraya_dbhouse" 
+--DATA.Translations[ ACT_MP_RELOAD_STAND ]				= "foc_jiraiya_oil_bombs"
+
+DATA.Translations[ ACT_MP_JUMP ]						= "wos_judge_a_idle"
+
+DATA.Translations[ ACT_LAND ]							= "wos_bs_shared_jump_land"
+
+wOS.AnimExtension:RegisterHoldtype( DATA )
