b76603ca3fe9478bc6aa59ebc6a1bf477cdabfc7
Lord-Meca <lordmeca255@gmail.com>
2024-12-04 23:22:54 +0100

    change medic mdl

--- lua/entities/ent_medic.lua
+++ lua/entities/ent_medic.lua
@@ -27,7 +27,7 @@ end
 function ENT:Initialize()
     if CLIENT then return end
 
-    self:SetModel("models/basalisk/basalisk.mdl")
+    self:SetModel("models/falko_naruto_foc/animal/katsuyu.mdl")
     self:SetSolid(SOLID_BBOX)
     self:SetMoveType(MOVETYPE_NONE)
     self:SetUseType(SIMPLE_USE)
